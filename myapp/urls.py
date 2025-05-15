37886ae90dbd68a16a75d46ffba8fff180d84a85
Subakeerthana1515 <subakeerthana1.com>
2025-05-15 12:53:34 +0530

    Updated code in feature-update branch

--- myapp/urls.py
+++ myapp/urls.py
@@ -24,7 +24,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('techjays/', include('techjays.urls')),  # Adjust for your app
-] 
+    
+    ] 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
