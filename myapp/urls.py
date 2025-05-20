d45cf8d349e0049f00af0dcfd909c959dc6d0f0e
Subakeerthana1515 <subakeerthana1@gmail.com>
2025-05-20 12:09:23 +0530

    Update urls.py
    urls corrected

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,12 +1,14 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from techjays import auth_views
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('techjays/', include('techjays.urls')),
-  
+    path('', auth_views.welcome, name='welcome'),
+
 
 ]
 
